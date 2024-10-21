baf8186eaf155e904dfe999ddaf57af3ef6aa48f
Sarah-Wiiliams <williams.goujigne@gmail.com>
2024-10-21 14:50:42 +0100

    initial commit

---
+++ week-one/calculator/calculator.js
@@ -0,0 +1,12 @@
+console.log('Welcome to my calculator project: WEEK ONE');
+
+//functions
+// conditionals (if-else statements)
+
+// once you are done, open the terminal and run these commands
+
+// git add .  (This saves the progress for github)
+
+// git commit -m "week one project"  (brief description of your changes)
+
+// git push   (Push changes to the web)
