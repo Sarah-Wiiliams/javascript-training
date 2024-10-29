b49f1d138123d559fa597e30c77603d97240181f
Sarah-Wiiliams <williams.goujigne@gmail.com>
2024-10-29 12:10:31 +0100

    Calculator app redone

---
+++ week-one/calculator/week-one.js
@@ -0,0 +1,95 @@
+
+
+// VARIABLE NAMING CONVENTIONS
+
+// snake_case --> last_name
+// camelCase --> lastName
+// variable names cannot start with a number --> 1day
+// variables can be uppercase
+// break....
+
+// FUNCTIONS AND CONDITIONALS
+
+// Functions are blocks of code, which enhance reusability; functions have parameters
+// Conditionals present multiple options depending on a condition.
+
+// concatenation
+
+
+// function greetings(first_name, last_name) {
+//     let full_name = `Hello ${first_name} ${last_name} from Caasitech front desk`
+//     console.log(full_name);
+// }
+
+// greetings('betrayer', 'sarah wills');
+// // greetings('another betrayer', 'giny wills');
+// // greetings('betrayee', 'alex');
+
+
+
+
+// function calculator(num1, num2, operation) {
+//     if (operation == "+") {
+//         console.log(num1 + num2);
+//     } else if (operation == '-') {
+//         console.log(num1 - num2);
+//     } else if (operation == '*') {
+//         console.log(num1 * num2);
+//     } else if (operation == '/') {
+//         console.log(num1 / num2);
+//     }
+// }
+
+// calculator(1, 30, '/');
+
+
+
+
+
+// let firstName = "Yannick";
+// let lastName = "Ceasar";
+// console.log('Happy day end', firstName);
+
+
+// let firstName = "Alex";
+// let lastName = "Traitor";
+// console.log('Hey', firstName, lastName);
+
+
+function calculator(num1, num2, operation) {
+    if (operation == "+") {
+        console.log(num1 + num2);
+    }else if (operation =='-') {
+        console.log(num1 - num2);
+    }else if (operation == '*') {
+        console.log(num1 * num2);
+    }else if (opration == '/') {
+        console.log(num1 * num2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
